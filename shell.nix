9edbcb9817e6e6ed3bb4b68a454eb82648be866c
William Silva <william.195675@fmm.org.br>
2025-09-07 21:17:56 -0400

    Telegram implementation

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{
+    pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+    name = "legal_assistant";
+    buildInputs = [
+        pkgs.uv
+        pkgs.python312
+    ];
+    shellHook = ''
+        echo "Welcome to Legal Assistant project!!!"
+    '';
+}
